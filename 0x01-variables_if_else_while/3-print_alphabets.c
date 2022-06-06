64b4495215130963e019c549552f9ea13936d43b
Mercymoy <Mercymoy@example.com>
2022-06-06 11:09:50 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,16 +6,16 @@
  * Return: Always 0.
  */
 int main(void)
-{
- char letter;
+{i
+char letter;
 
- for (letter = 'a'; letter <= 'z'; letter++)
-  putchar(letter);
+for (letter = 'a'; letter <= 'z'; letter++)
+putchar(letter);
 
- for (letter = 'A'; letter <= 'Z'; letter++)
-  putchar(letter);
+for (letter = 'A'; letter <= 'Z'; letter++)
+putchar(letter);
 
- putchar('\n');
+putchar('\n');
 
- return (0);
+return (0);
 }
