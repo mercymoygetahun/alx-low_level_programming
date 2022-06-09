86672195d7d269f489317eb270031560c40b3068
Mercymoy <Mercymoy@example.com>
2022-06-09 12:04:03 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,23 @@
  *
  * Return: Always 0.
  */
-int main(void)
+void print alphabet_x10(void)
 {
-	print_alphabet_x10();
-	return (0);
+	char ch;
+	int i;
+
+	i = 0;
+	
+	while (i < 10)
+	{
+		ch = 'a' ;
+		while(ch < = '2')
+	
+	
+	{
+		putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+	ch++;
 }
