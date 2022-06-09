aa495d91ad6eb23cc9c6255f8f80b55572e932ba
Mercymoy <Mercymoy@example.com>
2022-06-09 06:56:19 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,26 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * main-"main.h"
  *
- * Return: Alway 0 (Success)
+ * Return : 0
  */
+
 int main(void)
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+	char word[9] = "_putchar";
+
+	int i = 0;
+
+	while (i < 8)
+	{
+		putchar(word[i]);
+		i++;
+	}
+	_putchar("\n");
+
+	return (0);
+
 }
+
