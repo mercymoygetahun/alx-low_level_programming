03f6889f37320fe238bb682c27acbe46d67d0414
Mercymoy <Mercymoy@example.com>
2022-06-09 10:25:52 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet- print lower case alphabets
+ */
+void print_alphabet(void)
+{
+	char ch = 'a';
+	while (ch < = 'z' )
+	{ 
+		_putchar(ch");
+		ch++;
+	}
+	_putchar("\n");
+}
