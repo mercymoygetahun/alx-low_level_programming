b444c2a50c5c230636dd5b5ab5181a4252f19204
Mercymoy <Mercymoy@example.com>
2022-06-10 02:05:37 -0700

    Task 0 only

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,26 @@
-#include "main.h"
-
+#include <stdio.h>
 
 /**
- * main - Test function for postive or negative
- * return:0
- * /
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
-int i;
+	int i;
 
-i = 0;
-postive_or_negative(i);
+	printf("Infinite loop incoming :(\n");
 
-return (0);
+	i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+	}
+
+	printf("Infinite loop avoided! \\o/\n");
+
+	return (0);
 }
+
 
