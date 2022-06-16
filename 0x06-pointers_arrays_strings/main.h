13ea808474be5012c72332cf8004e150c4ce6fa2
Mercymoy <Mercymoy@example.com>
2022-06-16 06:06:39 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,24 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
+#include <stdio.h>
 
-int _putchar(char c);
+/**
+ * main - check the code
+ *
+ * Return: check the code 
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char s1[98] = "Hello ";
+	char s2[] = "World!\n";
+	char *ptr;
 
-#endif
+	printf("%s\n", s1);
+	printf("%s", s2);
+	ptr = _strcat(s1, s2);
+	printf("%s", s1);
+	printf("%s", s2);
+	printf("%s", ptr);
+	return (0);
+}
